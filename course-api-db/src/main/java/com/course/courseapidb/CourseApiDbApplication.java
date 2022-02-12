3828e31c6b045830b36acf8237f3eca49bd465fc
nayanabas <nayanabas@gmail.com>
2022-02-12 14:34:33 +0530

    course-api-db

---
+++ course-api-db/src/main/java/com/course/courseapidb/CourseApiDbApplication.java
@@ -0,0 +1,13 @@
+package com.course.courseapidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiDbApplication.class, args);
+	}
+
+}
---
+++ course-api-db/src/test/java/com/course/courseapidb/CourseApiDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.courseapidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
